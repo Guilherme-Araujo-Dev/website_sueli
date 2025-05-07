6659c1478060716737abeb44639eb01f2efb85c7
Guilherme-Araujo-Dev <guilhermearaujop26@gmail.com>
2025-05-07 00:05:02 -0300

    feat: inserção de algumas imagens para a tela

---
+++ src/js/carousel.js
@@ -0,0 +1,28 @@
+// <section class="carousel" aria-label="Gallery">
+//   <!-- <ol id="carousel-list"> -->
+//   <!--   <li> -->
+//   <!--     <img src="./src/assets/images/carousel/1.svg" alt="Imagem 1" /> -->
+//   <!--   </li> -->
+//   <!-- </ol> -->
+//
+//   <div class="carousel-container">
+//
+//     <!-- Left Button -->
+//     <button class="carousel-button" id="prev-button">
+//       <img class="botao-anterior" src="./src/assets/images/icons/chevron-left.svg" width="15px"
+//         alt="Seta para a esquerda" />
+//     </button>
+//
+//     <!-- Carousel Images-->
+//     <div class="carousel-list">
+//       <img class="carousel-image" src="./src/assets/images/carousel/1.svg" alt="Imagem 1" />
+//       <img class="carousel-image" src="./src/assets/images/carousel/2.svg" alt="Imagem 2" />
+//       <img class="carousel-image" src="./src/assets/images/carousel/3.svg" alt="Imagem 3" />
+//     </div>
+//
+//     <!-- Rigth Button -->
+//     <button class="carousel-button" id="next-button">
+//       <img class="botao-proximo" src="./src/assets/images/icons/chevron-right.svg" width="15"
+//         alt="Seta para a direita" />
+//     </button>
+// </section>
