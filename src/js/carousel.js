30ad0d5e949aac694598478c34d70c6eed5e9e76
Guilherme-Araujo-Dev <guilhermearaujop26@gmail.com>
2025-05-16 23:18:39 -0300

    feat: carrosel funcionando

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,28 +1,62 @@
-// <section class="carousel" aria-label="Gallery">
-//   <!-- <ol id="carousel-list"> -->
-//   <!--   <li> -->
-//   <!--     <img src="./src/assets/images/carousel/1.svg" alt="Imagem 1" /> -->
-//   <!--   </li> -->
-//   <!-- </ol> -->
-//
-//   <div class="carousel-container">
-//
-//     <!-- Left Button -->
-//     <button class="carousel-button" id="prev-button">
-//       <img class="botao-anterior" src="./src/assets/images/icons/chevron-left.svg" width="15px"
-//         alt="Seta para a esquerda" />
-//     </button>
-//
-//     <!-- Carousel Images-->
-//     <div class="carousel-list">
-//       <img class="carousel-image" src="./src/assets/images/carousel/1.svg" alt="Imagem 1" />
-//       <img class="carousel-image" src="./src/assets/images/carousel/2.svg" alt="Imagem 2" />
-//       <img class="carousel-image" src="./src/assets/images/carousel/3.svg" alt="Imagem 3" />
-//     </div>
-//
-//     <!-- Rigth Button -->
-//     <button class="carousel-button" id="next-button">
-//       <img class="botao-proximo" src="./src/assets/images/icons/chevron-right.svg" width="15"
-//         alt="Seta para a direita" />
-//     </button>
-// </section>
+// Objetos jQuery
+const $carouselItems = $('.carousel-item');
+const $prevButton = $('.prev-button');
+const $nextButton = $('.next-button');
+const $dot = $('.dot');
+
+let currentIndex = 0;
+let autoScroll;
+
+// Exibe as imagens do carrossel
+function showImage(index) {
+  currentIndex = index;
+
+  // Verifica se o índice está fora do intervalo
+  if (currentIndex < 0) {
+    currentIndex = $carouselItems.length - 1;
+  } else if (currentIndex >= $carouselItems.length) {
+    currentIndex = 0;
+  }
+
+  // Desliza a imagem atual e mostra a próxima
+  $carouselItems.css('transform', `translateX(-${currentIndex * 100}%)`);
+
+  // Atualiza os pontos
+  $dot.removeClass('active');
+  $dot.eq(currentIndex).addClass('active');
+}
+
+// Faz o scroll automático
+function startScroll() {
+  autoScroll = setInterval(() => {
+    showImage(currentIndex + 1);
+  }, 7000);
+}
+
+// Reseta o timeout do scroll automático
+function resetScroll() {
+  clearInterval(autoScroll);
+  startScroll();
+}
+
+// Adiciona o evento de clique a cada ponto
+$dot.click((event) => {
+  const idItem = $(event.target).attr('id').split('-')[1];
+
+  resetScroll();
+  showImage(idItem - 1);
+});
+
+// Adiciona os eventos de clique aos botões de navegação
+$prevButton.click(() => {
+  resetScroll();
+  showImage(currentIndex - 1);
+});
+
+$nextButton.click(() => {
+  resetScroll();
+  showImage(currentIndex + 1);
+});
+
+// Inicia o carrossel
+startScroll();
